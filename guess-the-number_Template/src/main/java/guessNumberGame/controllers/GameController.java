62511bf67424bf67947f15275045893945aa5af9
sarahthuang <sth2127@barnard.edu>
2022-11-23 16:53:49 -0500

    Completed guess the number project

---
+++ guess-the-number_Template/src/main/java/guessNumberGame/controllers/GameController.java
@@ -0,0 +1,75 @@
+package guessNumberGame.controllers;
+
+import guessNumberGame.Service.GameService;
+import guessNumberGame.data.GameDao;
+import guessNumberGame.data.RoundDao;
+import guessNumberGame.models.Game;
+import guessNumberGame.models.Round;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api")
+public class GameController {
+
+    private final GameDao gameDao;
+    private final RoundDao roundDao;
+
+
+    public GameController(GameDao gameDao, RoundDao roundDao) {
+        this.gameDao = gameDao;
+        this.roundDao = roundDao;
+    }
+
+    @PostMapping("/begin")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Game create() {
+        //implement create gameService object and game object
+        GameService gameService = new GameService();
+        Game game = gameService.newGame();
+
+        //add to database
+        gameDao.add(game);
+
+        //getGame will hide answer before returning it to the user
+        return gameService.getGames(game);
+    }
+
+
+    @PostMapping("/guess")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Round guessNumber(@RequestBody Round body) {
+        //implement
+        Game game = gameDao.findById(body.getGameId());
+        GameService gameService = new GameService();
+        Round round = gameService.guessNumber(game, body.getGuess(), gameDao);
+        return roundDao.add(round);
+    }
+
+    @GetMapping("/game")
+    public List<Game> all() {
+        //implement
+        List<Game> games = gameDao.getAll();
+        GameService gameService = new GameService();
+        gameService.getAllGames(games);
+        return games;
+    }
+
+    @GetMapping("game/{id}")
+    public Game getGameById(@PathVariable int id) {
+        //implement
+        Game game = gameDao.findById(id);
+        GameService gameService = new GameService();
+        return gameService.getGames(game);
+    }
+
+    @GetMapping("rounds/{gameId}")
+    public List<Round> getGameRounds(@PathVariable int gameId) {
+        //implement
+        return roundDao.getAllOfGame(gameId);
+    }
+
+}
